9cc4387ef680adb41b2d6a7796bd427a332cac51
Victor Leichsenring <victor.leichsenring@gmail.com>
2024-02-01 00:17:04 -0300

    final

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const userController = require('./controllers/user.controller');
+const authMiddleware = require('./middlewares/auth');
+const adminMiddleware = require('./middlewares/admin');
 // ...
 
 const app = express();
 
 // não remova ou mova esse endpoint
-app.post('/login', userController.login);
 
 app.get('/', (_request, response) => {
   response.send();
 });
-
+app.post('/login', userController.login);
+app.get('/me', authMiddleware, userController.getMe);
+app.get('/top-secret', authMiddleware, adminMiddleware, userController.topSecret);
 
 app.use(express.json());
 
